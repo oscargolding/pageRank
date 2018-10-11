d76f95ca949d13dbdf4dc844fd577ba2e9f1f2f9
oscargolding <oscargolding17@gmail.com>
2018-10-11 16:35:15 +1100

    Check

--- graph.c
+++ graph.c
@@ -2,6 +2,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 #include"graph.h"
 #include"readData.h"
 
@@ -28,7 +29,8 @@ struct baseVertex {
     int outGoing;
     int inComing;
     int length;
-    double pageRank;
+    float oldPageRank;
+    float pageRank;
     vNode node;
     vNode refs;
 };
@@ -38,6 +40,12 @@ static int inVertexList(bVertex given, int vertex);
 static vNode insertInside(bVertex list, int vertex);
 static vNode insertReference(bVertex list, int vertex);
 
+/* pageRank helper functions */
+static float pageSummation(Graph g, bVertex spec);
+static float weightedInLinks(bVertex j, bVertex i, Graph g);
+static float weightedOutLinks(bVertex j, bVertex i, Graph g);
+static float sumDiff(Graph g, int N);
+
 /* Generate a new graph with vertices, this graph is empty */
 Graph newGraph(int vertices) {
     /* Allocate memory for the graph */
@@ -46,8 +54,8 @@ Graph newGraph(int vertices) {
     g->nE = 0;
     g->connections = malloc(vertices * sizeof(bVertex));
     int i = 0;
-    double conver = (double)vertices;
-    double pVal = 1/conver;
+    float conver = (float)vertices;
+    float pVal = 1/conver;
     /* Appropriate process to create graph */
     while (i < vertices) {
 	g->connections[i] = malloc(sizeof(struct baseVertex));
@@ -57,6 +65,7 @@ Graph newGraph(int vertices) {
 	g->connections[i]->node = NULL;
 	g->connections[i]->refs = NULL;
 	g->connections[i]->pageRank = pVal;
+	g->connections[i]->oldPageRank = pVal;
 	i++;
     }
     return g;
@@ -106,7 +115,7 @@ void showGraph(Graph g) {
     while (i < g->nV) {
 	vNode current = g->connections[i]->node;
 	bVertex given = g->connections[i];
-	printf("%d has %d outgoing links, %d incoming and %.7lf pagerank\n", i,
+	printf("%d has %d outgoing links, %d incoming and %.9f pagerank\n", i,
 	       given->outGoing, given->inComing, given->pageRank);
 	if (current == NULL) printf("%d connects to nothing\n", i);
 	while (current != NULL) {
@@ -120,7 +129,7 @@ void showGraph(Graph g) {
     while (i < g->nV) {
 	vNode refCurr = g->connections[i]->refs;
 	bVertex given = g->connections[i];
-	printf("%d has %d outgoing links, %d incoming and %.7lf pagerank\n", i,
+	printf("%d has %d outgoing links, %d incoming and %.7f pagerank\n", i,
 	       given->outGoing, given->inComing, given->pageRank);
 	if (refCurr == NULL) printf("%d connects to nothing\n", i);
 	while (refCurr != NULL) {
@@ -130,4 +139,109 @@ void showGraph(Graph g) {
 	i++;
     }
 }
-    
+
+/* Calculate the pageRank of a graph */
+void calculatePageRank(Graph g, urlL list, float d, float diffPR,
+		       int maxIter) {
+    /* Need to set-up the algorithm accordingly */
+    int iteration = 0;
+    int N = getNoURL(list);
+    float precN = (float)N;
+    float fronTerm = (1-d)/precN;
+    float diff = diffPR;
+    int pageUsing = 0;
+    /* Start the iterations loop */
+    while ((iteration < maxIter) && (diff >= diffPR)) {
+	/* Need to get the pageSummation part */
+	float backRef = pageSummation(g, g->connections[pageUsing]);
+	float endTerm = d * backRef;
+	float newPageRank = fronTerm + endTerm;
+	float oldPageRank = g->connections[pageUsing]->pageRank;
+	g->connections[pageUsing]->oldPageRank = oldPageRank;
+	g->connections[pageUsing]->pageRank = newPageRank;
+	diff = sumDiff(g, N); if (diff < diffPR) break; 
+	iteration++;
+	pageUsing = (pageUsing == N-1) ? 0 : (pageUsing + 1);
+    }    
+    return;
+}
+
+/* Sum the difference between two graph points */
+static float sumDiff(Graph g, int N) {
+    int i = 0;
+    float sum = 0;
+    while (i < N) {
+	float res = fabs(g->connections[i]->pageRank -
+			 g->connections[i]->oldPageRank);
+	if (res < 0) {
+	    res = -res;
+	}
+	sum += res;
+	i++;	
+    }
+    return sum;
+}
+
+/* Helper function that assists in counting pageSummation */
+static float pageSummation(Graph g, bVertex spec) {
+    /* Set the reference nodes */
+    vNode refCurr = spec->refs;
+    /* Set a default value for the summation */
+    float defaultSum = 0;
+    while (refCurr != NULL) {
+	float pageRankRef = g->connections[refCurr->vertex]->pageRank;
+	float weightIn = weightedInLinks(g->connections[refCurr->vertex], spec,
+					  g);
+	float weightOut = weightedOutLinks(g->connections[refCurr->vertex],
+					    spec, g);
+	float mult = (pageRankRef * weightIn * weightOut);
+	defaultSum += mult;
+	refCurr = refCurr->next;
+    }
+    return defaultSum;
+}
+
+/* Helper to find the weighted inLinks */
+static float weightedInLinks(bVertex j, bVertex i, Graph g) {
+    /* Getting the top term to use for algorithm */
+    int inLinkI = i->inComing;
+    /* if (inLinkI == 0) return 0.0; */
+    /* Start getting the outgoing from vertex j */
+    vNode current = j->node;
+    float sum = 0;
+    while (current != NULL) {
+	sum += g->connections[current->vertex]->inComing;
+	current = current->next;
+    }
+    float conTop = (float)inLinkI;
+    float conBot = sum;
+    float res = conTop/conBot;
+    return res;
+}
+
+/* Helper to find the weighted outLinks */
+static float weightedOutLinks(bVertex j, bVertex i, Graph g) {
+    /* Getting the top term to use for algorithm */
+    int outLinkI = i->outGoing;
+    /* Start getting the outgoing from vertex j */
+    vNode current = j->node;
+    float sum = 0;
+    while (current != NULL) {
+	float val = (float)g->connections[current->vertex]->outGoing;
+	if (val == 0) {
+	    val = 0.5;
+	}
+	sum += val;
+	current = current->next;
+    }
+    float conTop = (float)outLinkI;
+    if (conTop == 0) {
+	conTop = 0.5;
+    }
+    float conBot = sum;
+    if (conBot == 0) {
+	conBot = 0.5;	
+    }
+    float res = conTop/conBot;
+    return res;
+}
--- pagerank.c
+++ pagerank.c
@@ -11,13 +11,15 @@ int main (int argc, char *argv[]) {
 	fprintf(stderr, "Program requires three arguments\n");
 	exit(1);
     }
-    double d = strtod(argv[1], NULL);
-    double diffPR = strtod(argv[2], NULL);
+    float d = strtod(argv[1], NULL);
+    float diffPR = strtod(argv[2], NULL);
     int maxIter = strtol(argv[3], NULL, 10);
     printf("The d:%lf, diffPR:%lf, maxIter:%d\n", d, diffPR, maxIter);
     urlL getList = getConnections();
     printf("%s\n", returnURL(0, getList));
     Graph g = getGraph(getList);
+    showGraph(g);
+    calculatePageRank(g, getList, d, diffPR, maxIter);
     showGraph(g);
     return EXIT_SUCCESS;
 }
